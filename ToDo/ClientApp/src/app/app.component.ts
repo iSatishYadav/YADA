24150e2c07abcd96920c73ca803fab09a052b7e6
Satish Yadav <monstersatish@live.com>
2019-04-04 18:52:20 +0530

    Assets added. YADA.

--- ToDo/ClientApp/src/app/app.component.ts
+++ ToDo/ClientApp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'YATA - Yet Another TODO App';
+  title = 'YADA - Yet Another to DO App';
 }
